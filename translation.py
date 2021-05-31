2afec0653eacbec7d386bf4b039e575c91340f4b
theed221120 <85099920+theed221120@users.noreply.github.com>
2021-05-31 21:21:19 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -2,8 +2,8 @@ import os
 from config import Config
 
 class Translation(object):
-  START_TXT = """<b>Hai {}!!</b>
-<i>I'm Simple Auto file Forward Bot
+  START_TXT = """<b>ഹലോ🙃 {}!!</b>
+<i>Only one thing counts in this life – get them to sign on the line that is dotted 😁😁 I'm Simple Auto file Forward Bot
 This Bot forward all files to One Public channel to Your Personal channel
 More details /help</i>"""
   CAPTION = "`{}`\n\n" + str(Config.CAPTION)
@@ -11,18 +11,18 @@ More details /help</i>"""
 <b>• Currectly fill your Heroku Config vars</b> <code>FROM_CHANNEL</code> and <code>TO_CHANNEL</code> <b>and other Vars</b>
 <b>• Then give admin permission in your personal telegram channel</b>
 <b>• Then send any message In your personal telegram channel</b>
-<b>• Then use /run command in your bot</b>
+<b>• Then use /run command in your bot നിനക്ക് പറ്റുമോ എന്ന് ചുമ്മാ നോക്ക് 😉</b>
 
 <b><u>Available Command</b></u>
 
-* /start - <b>Bot Alive</b>
-* /help - <b>more help</b>
+* /start - <b>ബോട്ടിന് ജീവൻ ഉണ്ടോ എന്ന് നോക്കാന്‍</b>
+* /help - <b>നിനക്ക് വല്ല സംശയവും ഉണ്ടെങ്കില്‍ നോക്കാന്‍</b>
 * /run - <b>start forward</b>
-* /about - <b>About Me</b>"""
+* /about - <b>ഞാൻ ആരാണ് എന്താണ് എന്ന് അറിയാൻ</b>"""
   ABOUT_TXT = """<b><u>My Info</b></u>
 
-<b>Name :</b> <code>Auto Forward Bot</code>
-<b>Credit :</b> <code>Dark Angel</code>
+<b>Name :</b> <code>File Forward SOMAN</code>
+<b>Credit :</b> <code>@hsk_the_editor</code>
 <b>Language :</b> <code>Python3</code>
 <b>Lybrary :</b> <code>Pyrogram v1.2.9</code>
 <b>Server :</b> <code>Heroku</code>
