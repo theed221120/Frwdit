d7e3d1e996e440bc2fdbc88bbbf34add19b996d5
theed221120 <85099920+theed221120@users.noreply.github.com>
2021-05-31 21:02:50 +0530

    Update stoping.py

--- plugins/stoping.py
+++ plugins/stoping.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) Dark Angel
+# (c) hsk the editor
 
 import os
 import sys
@@ -13,11 +13,11 @@ async def stop_button(c: Client, cb: CallbackQuery):
     await cb.message.delete()
     await cb.answer()
     msg = await c.send_message(
-        text="<i>Trying To Stoping.....</i>",
+        text="<i>Trying To Stop Forwarding.....</i>",
         chat_id=cb.message.chat.id
     )
     await asyncio.sleep(5)
-    await msg.edit("<i>File Forword Stoped Successfully 👍</i>")
+    await msg.edit("<i>File Forword Stoped Successfully കഴിഞ്ഞു😁👍</i>")
     os.execl(sys.executable, sys.executable, *sys.argv)
     
 @Client.on_callback_query(filters.regex(r'^close_btn$'))
